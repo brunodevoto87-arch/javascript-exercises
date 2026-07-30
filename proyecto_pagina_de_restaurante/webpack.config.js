19c413cb23345f631f41c2ee91e31f26cf22b159
brunodevoto <brunodevoto87@gmail.com>
2026-07-30 20:35:28 +0000

    Fix GitHub Pages routing for restaurant project

--- proyecto_pagina_de_restaurante/webpack.config.js
+++ proyecto_pagina_de_restaurante/webpack.config.js
@@ -8,7 +8,7 @@ export default {
         filename: "main.js",
         path: path.resolve(import.meta.dirname, "dist"),
         clean: true,
-        publicPath: "",
+        publicPath: "./",
     },
     devtool: "eval-source-map",
     devServer:{
