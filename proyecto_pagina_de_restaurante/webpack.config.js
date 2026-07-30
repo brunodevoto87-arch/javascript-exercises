e6b418a5bba3b807f6cf8bf82a9b52a1aa5346b7
brunodevoto <brunodevoto87@gmail.com>
2026-07-30 20:11:51 +0000

    Cambio webpack a modo production para GitHub Pages

--- proyecto_pagina_de_restaurante/webpack.config.js
+++ proyecto_pagina_de_restaurante/webpack.config.js
@@ -2,12 +2,13 @@ import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-    mode: "development",
+    mode: "production",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(import.meta.dirname, "dist"),
         clean: true,
+        publicPath: "",
     },
     devtool: "eval-source-map",
     devServer:{
